2d3e721827990e4863f0b63b872c49657c5c8e17
luisgilberto1995 <luisgil95crash@gmail.com>
2018-10-27 19:09:57 -0600

    metodos para publicar listos

--- app.js
+++ app.js
@@ -13,14 +13,18 @@ var config = {
 var REF_PUBLICATIONS = firebase.database().ref('publicacion');
 
 const express = require('express');
+const cors = require("cors");
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(bodyParser.json());
+
+app.use(cors({origin:true}));
 
 app.get('/',function (req,res)
 {
@@ -32,26 +36,79 @@ app.listen(3005,function()
     console.log('Servidor iniciado');
 });
 
-app.post('/postPublication', (req, res) => {
-
+app.post('/postPublication', async (req, res) => {
 
     var type = req.query.type;/* este sirve cuando pasas un parametro por url era el type que yo les pedia en lo de las colonias,
                                 se pueden poner n parámetros y es como el body que te explice req.query.nombreparametro
                                 */
-
     var body = req.body; // este es el body que siempre jalo al principio para no estar llamando a cada rato a req.body
+    
     /*firebase.database().ref("/").once('value').then(function(snapshot)
     {
         console.log("Then:");
         console.log(snapshot.val());
     });
     */
+   //VALIDAR HASHTAGS NO OFENSIVOS
+   //VALIDAR IMAGEN
 
-    console.log(body.id);
-    console.log(body.nombre);
+   console.log("Publicando...");
+   let fecha = new Date();
+   var xhr = new XMLHttpRequest();
+   var url = "http://35.227.59.137:3005/ping";
+   xhr.open("POST", url, true);
+   xhr.setRequestHeader("Content-Type", "application/json");
+   xhr.onreadystatechange = function () 
+   {
+       if (xhr.readyState === 4 && xhr.status === 200) 
+       {
+           //LEYENDO RESPUESTA APIVISION
+           var visionAnswer = xhr.responseText;
+           console.log("Respuesta API Vision: "+visionAnswer);
+           if(visionAnswer === "OK")
+           {
+                let publication  = {};
+                publication.contenido = body.contenido;
+                publication.imagen = body.imagen;
+                publication.dislikes = 0;
+                publication.likes = 0;
+                publication.fecha = fecha.getTime();
+                let hashtags = body.hashtags;
+                //console.log(hashtags);
+                publication.usuario = body.usuario;
+                saveFirebase(REF_PUBLICATIONS, publication);
+                res.send("{\"estado\":true}");
+           }
+           else
+           {
+                res.send("{\"estado\":false}");
+           }
+       }
+   };
+   console.log("Inicio el test API Vision...");
+   var imagen = {};
+   imagen.url = body.imagen;
+   imagen.name = "x.jpg";
+   var data = JSON.stringify(imagen);
+   xhr.send(data);
+});
 
-    res.send("todo bien");
 
+app.post('/deletePublication', async (req, res) => {
+
+    var type = req.query.type;/* este sirve cuando pasas un parametro por url era el type que yo les pedia en lo de las colonias,
+                                se pueden poner n parámetros y es como el body que te explice req.query.nombreparametro
+                                */
+    var body = req.body; // este es el body que siempre jalo al principio para no estar llamando a cada rato a req.body
+    var id = body.id;
+
+    
+    deleteFirebase(REF_PUBLICATIONS, id);
+
+    console.log("Eliminando...");
+
+    res.send("{\"estado\":true}");
+                
 });
 
 function json_converter( response, error, access) {
@@ -62,3 +119,17 @@ function json_converter( response, error, access) {
 
 }
 
+async function saveFirebase(reference, object)
+{
+    await reference.push(object);
+    console.log("Guardado!");
+    return true;
+}
+
+
+async function deleteFirebase(reference, id)
+{
+    await reference.child(id).remove();
+    console.log("Borrado!");
+    return true;
+}
